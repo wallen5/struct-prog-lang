367f45ce608e75a8812e901575a3a479e6f93f93
Will Allen <wallen5@kent.edu>
2026-01-22 11:41:32 -0500

    lecture 2

--- personal/topic-01-simple-expressions/tokenizer.py
+++ personal/topic-01-simple-expressions/tokenizer.py
@@ -1,8 +1,35 @@
 import re
 
-p = re.compile("ab*")
+#p = re.compile("ab*")
 
-if p.match("a") :
-    print("match")
-else:
-    print("not match")
+#if p.match("a") :
+#    print("match")
+#else:
+#    print("not match")
+
+patterns = [
+    (r"s\+", "whitespace"),
+    (r"\d+", "number"),
+    (r"\+", "+"),
+    (r"\-", "-"),
+    (r"\/", "/"),
+    (r"\*", "*"),
+    (r".", "error"),
+    ]
+
+patterns = [{re.compile(p), tag} for p,tag in patterns]
+
+def tokenize(characters):
+    "Tokenize a string using the patterns above"
+    tokens = []
+    position = 0
+    line = 1
+    column = 1
+
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match is not None
+        
